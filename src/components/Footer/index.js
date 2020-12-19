8ac53763cb8eae0b077f992543ed4e324ef731ea
shamona95 <shamona9594@gmail.com>
2020-12-19 15:58:00 +0300

    header footer components added... worked on folder structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span className="footer-text">| This is a sample application. |</span>
+    </div>
+  );
+}
---
+++ src/containers/App/index.js
@@ -0,0 +1,12 @@
+import "./app.scss";
+import ProductPage from "../ProductPage";
+
+function App() {
+  return (
+    <div>
+      <ProductPage />
+    </div>
+  );
+}
+
+export default App;
